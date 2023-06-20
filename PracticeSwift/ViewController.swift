18cb9d8ea5f694f4a1c1c69aea9cd119ae0a416c
LeeeeSuHyeon <tngus0673@naver.com>
2023-06-20 18:02:37 +0900

    Initial Commit

---
+++ PracticeSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeSwift
+//
+//  Created by 이수현 on 2023/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
