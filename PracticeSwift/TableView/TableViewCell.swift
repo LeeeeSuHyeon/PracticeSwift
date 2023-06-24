aace85cefa6749f25876c5a3c6fca11786e65dcd
LeeeeSuHyeon <tngus0673@naver.com>
2023-06-25 00:02:28 +0900

    테이블뷰 연습

---
+++ PracticeSwift/TableView/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  PracticeSwift
+//
+//  Created by 이수현 on 2023/06/24.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
