0cae92633df9f7e9c12b906db20a509918d77b7f
LeeeeSuHyeon <tngus0673@naver.com>
2023-06-30 16:35:36 +0900

    Session을 이용한 RESTful API 연습

---
+++ PracticeSwift/API/Api.swift
@@ -0,0 +1,45 @@
+//
+//  Api.swift
+//  PracticeSwift
+//
+//  Created by 이수현 on 2023/06/30.
+//
+
+import Foundation
+
+// URL 생성
+guard let url = URL(string: "https://www.example.com/data.json") else {
+    print("유효한 URL을 생성할 수 없습니다.")
+    return
+}
+
+// URL 세션 생성
+let session = URLSession.shared
+
+// 데이터 다운로드 작업 생성
+let task = session.dataTask(with: url) { (data, response, error) in
+    // 네트워크 요청이 완료된 후 실행되는 클로저
+
+    // 에러 처리
+    if let error = error {
+        print("데이터 다운로드 중 에러 발생: \(error)")
+        return
+    }
+
+    // 응답 확인
+    guard let httpResponse = response as? HTTPURLResponse,
+          (200...299).contains(httpResponse.statusCode) else {
+        print("유효한 HTTP 응답이 아닙니다.")
+        return
+    }
+
+    // 데이터 확인
+    if let data = data {
+        // 데이터를 원하는 방식으로 처리합니다.
+        // 예를 들어, JSON 데이터를 파싱하거나 파일로 저장할 수 있습니다.
+        print("다운로드된 데이터: \(data)")
+    }
+}
+
+// 작업 시작
+task.resume()
--- PracticeSwift/AppDelegate.swift
+++ PracticeSwift/AppDelegate.swift
@@ -32,44 +32,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     
-    // 앱이 실행되고 초기 설정이 완료된 후 호출
-    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        print("willFinishLaunchingWithOptions : 앱이 실행되고 초기 설정이 완료된 후 호출")
-        return true
-    }
-    
-    // 앱이 활성화되어 사용자와 상호작용이 가능한 상태가 되었을 때 호출
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        print("applicationDidBecomeActive : 앱이 활성화되어 사용자와 상호작용이 가능한 상태가 되었을 때 호출")
-    }
-    
-    
-    
-    //실행 준비가 모두 끝나고 화면이 사용자에게 보여지기 직전에 호출
-    func applicationDidFinishLaunching(_ application: UIApplication) {
-        print("applicationDidFishLaunching : 앱 실행 준비가 모두 끝나고 화면이 사용자에게 보여지기 직전 호출")
-    }
-    
-
-    // 앱이 비활성화되고 포그라운드에서 백그라운드로 전환되기 직전에 호출
-    func applicationWillResignActive(_ application: UIApplication) {
-        print("applicationWillResignActive : 앱이 비활성화되고 포그라운드에서 백그라운드로 전환되기 직전에 호출")
-    }
-    
-    // 백그라운드로 전환되었을 때 호출
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        print("applicationDidEnterBackground : 백그라운드로 전환되었을 때 호출")
-    }
-
-    
-    // 백그라운드에서 포그라운드로 전환될 때 호출
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        print("applicationWillEnterForeground : 백그라운드에서 포그라운드로 전환될 때 호출")
-    }
-    
-    // 앱이 종료되기 직전에 호출
-    func applicationWillTerminate(_ application: UIApplication) {
-        print("applicationWillTerminate : 앱이 종료되기 직전에 호출")
-    }
+//    // 앱이 실행되고 초기 설정이 완료된 후 호출
+//    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//        print("willFinishLaunchingWithOptions : 앱이 실행되고 초기 설정이 완료된 후 호출")
+//        return true
+//    }
+//
+//    // 앱이 활성화되어 사용자와 상호작용이 가능한 상태가 되었을 때 호출
+//    func applicationDidBecomeActive(_ application: UIApplication) {
+//        print("applicationDidBecomeActive : 앱이 활성화되어 사용자와 상호작용이 가능한 상태가 되었을 때 호출")
+//    }
+//
+//
+//
+//    //실행 준비가 모두 끝나고 화면이 사용자에게 보여지기 직전에 호출
+//    func applicationDidFinishLaunching(_ application: UIApplication) {
+//        print("applicationDidFishLaunching : 앱 실행 준비가 모두 끝나고 화면이 사용자에게 보여지기 직전 호출")
+//    }
+//
+//
+//    // 앱이 비활성화되고 포그라운드에서 백그라운드로 전환되기 직전에 호출
+//    func applicationWillResignActive(_ application: UIApplication) {
+//        print("applicationWillResignActive : 앱이 비활성화되고 포그라운드에서 백그라운드로 전환되기 직전에 호출")
+//    }
+//
+//    // 백그라운드로 전환되었을 때 호출
+//    func applicationDidEnterBackground(_ application: UIApplication) {
+//        print("applicationDidEnterBackground : 백그라운드로 전환되었을 때 호출")
+//    }
+//
+//
+//    // 백그라운드에서 포그라운드로 전환될 때 호출
+//    func applicationWillEnterForeground(_ application: UIApplication) {
+//        print("applicationWillEnterForeground : 백그라운드에서 포그라운드로 전환될 때 호출")
+//    }
+//
+//    // 앱이 종료되기 직전에 호출
+//    func applicationWillTerminate(_ application: UIApplication) {
+//        print("applicationWillTerminate : 앱이 종료되기 직전에 호출")
+//    }
 }
 
