26fd622d4551f08a320a80de1700e76ef8527f7d
LeeeeSuHyeon <tngus0673@naver.com>
2023-07-12 15:18:30 +0900

    flatMap, compactMap 연습

--- PracticeSwift/HigherOrderFunction/HigherOrderFunction.playground/Contents.swift
+++ PracticeSwift/HigherOrderFunction/HigherOrderFunction.playground/Contents.swift
@@ -36,11 +36,39 @@ import UIKit
 
 
 // reduce 함수
-let numbers = [1, 2, 3, 4, 5]
-let sum = numbers.reduce(0) { $0 + $1 }
-print(sum) // 출력: 15
+//let numbers = [1, 2, 3, 4, 5]
+//let sum = numbers.reduce(0) { $0 + $1 }
+//print(sum) // 출력: 15
+//
+//
+//let words = ["Hello", " ", "World", "!"]
+//let sentence = words.reduce("") { $0 + $1 }
+//print(sentence) // 출력: "Hello World!"
 
 
-let words = ["Hello", " ", "World", "!"]
-let sentence = words.reduce("") { $0 + $1 }
-print(sentence) // 출력: "Hello World!"
+
+
+
+// compactMap 함수
+let stringNumbers = ["1", "2", "3", "four", "5"]
+let numbers = stringNumbers.compactMap { Int($0) }
+print(numbers) // 출력: [1, 2, 3, 5]
+
+// map 함수
+let mapNumbers = stringNumbers.map { Int($0) }
+print(mapNumbers) // 출력: [Optional(1), Optional(2), Optional(3), nil, Optional(5)]
+
+
+
+// flatMap 함수
+let nestedArray = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
+let flattenedArray = nestedArray.flatMap { $0 }
+print(flattenedArray) // 출력 : [[1, 2], [3, 4], [5, 6], [7, 8]]
+
+
+let doubleFalttenArray = flattenedArray.flatMap{$0}
+print(doubleFalttenArray) // 출력: [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+let flattenMap = nestedArray.flatMap{ $0.flatMap{ $0 }}
+print(flattenMap)  // 출력: [1, 2, 3, 4, 5, 6, 7, 8]
