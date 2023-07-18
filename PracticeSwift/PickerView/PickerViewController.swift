4eaf0455c663f75ce4614a7ad9a4f2387130bbcd
LeeeeSuHyeon <tngus0673@naver.com>
2023-07-18 16:52:37 +0900

    피커뷰 연습 중

--- PracticeSwift/PickerView/PickerViewController.swift
+++ PracticeSwift/PickerView/PickerViewController.swift
@@ -11,6 +11,14 @@ class PickerViewController: UIViewController, UIPickerViewDelegate, UIPickerView
 
     @IBOutlet weak var pickerView: UIPickerView!
     
+    
+    let pickerViewCount = 5     // pickerView의 Lable 개수
+    let pickerViewColumn = 1    // pickerView의 열 개수
+    
+    // pickerView에 띄울 내용
+    var pickerViewList = ["홍길동", "이길동", "김길동", "최길동", "박길동"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
