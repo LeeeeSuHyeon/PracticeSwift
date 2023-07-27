dc8827cb40ec367d2088f04795505ac844384e6e
LeeeeSuHyeon <tngus0673@naver.com>
2023-07-27 23:41:19 +0900

    HStack 연습 중

--- PracticeSwift/Stack/HStack.swift
+++ PracticeSwift/Stack/HStack.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MyHStack: View {
     var body: some View {
+        
         HStack{
             Text("HStack")
 
@@ -20,7 +21,23 @@ struct MyHStack: View {
                 .foregroundColor(.green)
                 .frame(width: 100, height: 100)
             
+            VStack{
+                Text("Stack in HStack")
+                    .font(.system(size: 10))
+                
+                Rectangle()
+                    .foregroundColor(.red)
+                    .frame(width: 50, height: 50)
+                
+                Rectangle()
+                    .foregroundColor(.blue)
+                    .frame(width: 50, height: 50)
+            }
+            
         }
+        .frame(height: 300)
+        .background(Color(.orange))
+        
                 
     }
 }
