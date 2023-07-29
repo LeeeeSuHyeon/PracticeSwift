3dd2a09d9158b80ace5b9ed5d1168c63f94aad0a
LeeeeSuHyeon <tngus0673@naver.com>
2023-07-29 17:37:07 +0900

    YPImageViewPicker 앨범 가져오기

--- PracticeSwift/Picture/PictureViewController.swift
+++ PracticeSwift/Picture/PictureViewController.swift
@@ -18,7 +18,8 @@ class PictureViewController: UIViewController {
         super.viewDidLoad()
         
         // 이미지뷰와 버튼의 cornerRadius 설정
-        self.imageView.layer.cornerRadius = self.imageView.frame.height / 2
+        self.imageView.layer.cornerRadius = imageView.frame.height / 2
+        self.imageView.contentMode = .scaleAspectFill
         self.button.layer.cornerRadius = 10
         
         // button Action 추가
@@ -34,8 +35,9 @@ class PictureViewController: UIViewController {
         // YPImagePickerConfiguration 인스턴스 생성
         var config = YPImagePickerConfiguration()
         
-        // 화면 설정
-        config.screens = [.library, .video, .photo]
+        // 화면 설정 - 시물레이터를 사용하기 때문에 library만 사용하겠음
+//        config.screens = [.library, .video, .photo]
+        config.screens = [.library]
         
         // YPImagePicker 인스턴스 생성
         let picker = YPImagePicker(configuration: config)
@@ -51,7 +53,7 @@ class PictureViewController: UIViewController {
                 print(photo.modifiedImage) // 변환된 이미지, nil일 수도 있음
                 print(photo.exifMeta) // 원본 이미지의 Exif 메타데이터 출력
                 
-                // 이미지뷰의 이미지를 photo.image로 설정 
+                // 이미지뷰의 이미지를 photo.image로 설정
                 self.imageView.image = photo.image
             } // end of photo
             
