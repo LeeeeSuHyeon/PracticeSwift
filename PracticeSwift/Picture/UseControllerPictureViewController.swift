7a03eaba58dba80cb729f3048e1ae1add4df0be2
LeeeeSuHyeon <tngus0673@naver.com>
2023-08-03 23:51:10 +0900

    UIImagePickerController 연습 중

--- PracticeSwift/Picture/UseControllerPictureViewController.swift
+++ PracticeSwift/Picture/UseControllerPictureViewController.swift
@@ -13,23 +13,18 @@ class UseControllerPictureViewController: UIViewController {
     @IBOutlet weak var profileImageView: UIImageView!
     
     
-    // 버튼 액션 
-    @IBAction func changeProfileBtn(_ sender: UIButton) {
-        btnAction()
-        
 
-    }
     
-    // 이미지 선택 완료 시 호출되는 메서드
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let selectedImage = info[.originalImage] as? UIImage {
-            // 이미지뷰에 선택한 이미지 설정
-            imageView.image = selectedImage
-        }
-        
-        // 이미지 피커 닫기
-        picker.dismiss(animated: true, completion: nil)
-
+//    // 이미지 선택 완료 시 호출되는 메서드
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        if let selectedImage = info[.originalImage] as? UIImage {
+//            // 이미지뷰에 선택한 이미지 설정
+//            imageView.image = selectedImage
+//        }
+//
+//        // 이미지 피커 닫기
+//        picker.dismiss(animated: true, completion: nil)
+//
 
     
     override func viewDidLoad() {
@@ -39,15 +34,27 @@ class UseControllerPictureViewController: UIViewController {
     }
     
 
+    
 }
 
 
 extension UseControllerPictureViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    fun btnAction(){
+    // 버튼 액션
+    @IBAction func changeProfileBtn(_ sender: UIButton) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
-        imagePicker.mediaTypes = ["public.image"]
-        present(imagePicker, animated: true, completion: nil)
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true)
+    }
+    
+    
+    // 이미지 선택 완료 시 호출되는 메서드
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // 선택한 이미지 가져오기
+        if let selectedImage = info[.originalImage] as? UIImage {
+            // 이미지뷰에 선택한 이미지 설정
+            profileImageView.image = selectedImage
+        }
     }
 }
