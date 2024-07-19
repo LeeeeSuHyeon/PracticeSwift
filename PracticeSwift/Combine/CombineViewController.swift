79be8bc7b576e0b809dd8c7f3c6781d3b08826ce
suhyeon lee <tngus0673@naver.com>
2024-07-19 22:56:12 +0900

    Combine 예제 Storyboard

---
+++ PracticeSwift/Combine/CombineViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CombineViewController.swift
+//  PracticeSwift
+//
+//  Created by 이수현 on 7/19/24.
+//
+
+import UIKit
+
+class CombineViewController : ViewController {
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtCheckPassword: UITextField!
+    @IBOutlet weak var btn: UIButton!
+    
+    override func viewDidLoad() {
+        
+    }
+}
